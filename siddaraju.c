f80aade3bc7a69349ba074d90596d0453ba56743
roopa <roopashreep2006@gmail.com>
2024-08-27 01:21:27 -0700

    intial commit

---
+++ siddaraju.c
@@ -0,0 +1,19 @@
+<html>
+<body>
+<script>
+var,n1.n2,res;
+n1=propt("enter the n1 value");
+n1=parse(n1);
+n2=prompt("enter the n2 value");
+n2=parse(n2);
+res=n1+n2;
+("addition="+res+"<br>");
+res=n1-n2;
+("subtract="+res+"<br>");
+res=n1*n2;
+("muplity="+res+"<br>");
+res=n1/n2;
+("divide="+res+"<br>");
+</script>
+</body>
+</html>
